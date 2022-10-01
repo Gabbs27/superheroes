b8159561b4e447fc7f80fc291deeac4def8aa7d3
Gabriel <djabreu_@hotmail.com>
2022-09-30 23:10:55 -0400

    Finalizado, se crean carpetas views y partials para utilizar Hbs y reutilizar componentes.

--- app.js
+++ app.js
@@ -21,23 +21,48 @@
 // //   res.send("Hello world"); //Send hello world to this route.
 // // });
 
-//Express pkg
+//--------------------------------------- //Using HBS
+// //Express pkg
+// const express = require("express");
+// //Path module
+// const path = require("path");
+// //Using express
+// const app = new express();
+// //using HBS
+// const hbs = require("hbs");
+
+// app.listen(3000, (req, res) => {
+//   console.log("ServerRunnin en el 3000");
+// });
+// app.set("view engine", "hbs");
+// app.set("views", path.join(__dirname, "/views"));
+
+// app.get("/", (req, res) => {
+//   res.render("index", {
+//     author: "Gabo",
+//   });
+// });
+
 const express = require("express");
-//Path module
-const path = require("path");
-//Using express
-const app = new express();
-//using HBS
 const hbs = require("hbs");
-
-app.listen(3000, (req, res) => {
-  console.log("ServerRunnin en el 3000");
-});
+const path = require("path");
+const app = express();
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "/views"));
-
+app.set("views", path.join(__dirname, "/templates/views"));
+//RegisterPartials
+hbs.registerPartials(path.join(__dirname, "/templates/partials"));
 app.get("/", (req, res) => {
   res.render("index", {
-    author: "Gabo",
+    title: "Home",
+    name: "Arash Arora",
   });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Arash Arora",
+  });
+});
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
